a3e13f34c70e79ea52eabfe84e081e972097b642
Elayachi Hamrit <142249997+elayachihamritoff@users.noreply.github.com>
2023-10-31 02:11:57 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -23,21 +23,21 @@
 		let theWinner = checkWinner(playerChoice, computerChoice);
 
 
-		//Update
 
+		//Update
 		
-		
-		if (theWinner == 'Player') {
+		if (playerScore == 5 || computerScore == 5 ) {
+			endGame();
+		}
+		else if (theWinner == 'Player') {
 		
 			playerScore++;
 			playerScoreDisplay.textContent = playerScore;
 			resultDisplay.textContent = `You win`;
 			resultDisplayWrapper.style.backgroundColor = 'var(--light-green)';
 			resultDisplay.style.color = 'var(--dark-green)';
-
-	;
-
 		} 
+
 		else if (theWinner == 'Computer'){
 			computerScore++;
 			computerScoreDisplay.textContent = computerScore;
@@ -48,7 +48,9 @@
 
 		
 		} else {
-
+			resultDisplay.textContent = `Keep going!`;
+			resultDisplayWrapper.style.backgroundColor = 'var(--light_orange)';
+			resultDisplay.style.color = 'var(--dark_orange)'
 		}
 
 	}
@@ -75,5 +77,22 @@
 	};
 
 
+	function endGame() {
+
+		if (playerScore === 5 ) {
+			resultDisplay.textContent = `VICTORY`;
+			resultDisplayWrapper.style.backgroundColor = 'var(--light_blue)';
+			resultDisplay.style.color = 'var(--dark_blue)';
+		
+		} else {
+
+			resultDisplay.textContent = `DEFEAT`;
+			resultDisplayWrapper.style.backgroundColor = 'var(--light-red)';
+			resultDisplay.style.color = 'var(--dark-red)';
+		}
+	}
+
+	
 	options.forEach((option) => option.addEventListener('click', playGame ));	
+
 
